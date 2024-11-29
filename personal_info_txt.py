a6a849391f4a662fda1db5ae5009d21df6360ea1
Adrian Ranel <adrianranelm@gmail.com>
2024-11-30 00:44:47 +0800

    creat txt, added counter, first loop

--- personal_info_txt.py
+++ personal_info_txt.py
@@ -1,7 +1,11 @@
 # create a txt file to store data
+info_file = open ("./user_info_list.txt", "w")
+input_counter = 0
 # loop to ask user if they want to enter more information
+while True:
 # loop for 5 types of information
 # insert user inputs into txt
 # close loop 1
 # close loop 2
 # close file
+    break
