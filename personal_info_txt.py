b6d1410b18a8e60280c9cf004509fe8922727929
Adrian Ranel <adrianranelm@gmail.com>
2024-11-30 00:27:45 +0800

    pseudcode

---
+++ personal_info_txt.py
@@ -0,0 +1,7 @@
+# create a txt file to store data
+# loop to ask user if they want to enter more information
+# loop for 5 types of information
+# insert user inputs into txt
+# close loop 1
+# close loop 2
+# close file
