24b90746e36f6830bddd0ec237a471a48c40f215
Adrian Ranel <adrianranelm@gmail.com>
2024-11-30 01:43:41 +0800

    added user conter and fixed standard

--- personal_info_txt.py
+++ personal_info_txt.py
@@ -1,18 +1,16 @@
 # create a txt file to store data
+user_count = 0
 with open ("./user_info_list.txt", "w") as info_file:
 
 # loop to ask user if they want to enter more information
 
     while True:
+#Count amount of users
+        user_count += 1
+        print(f"User {user_count}: ")
 
-# ask user for 5 types of information
+        '''ask user for 5 types of information'''
 
-            
-
-                
-
-
-# insert user inputs into txt
 # close loop 
         will_add = input("Would you like to continue<Yes/No> :")
         will_add = will_add.lower()
@@ -21,4 +19,3 @@ with open ("./user_info_list.txt", "w") as info_file:
             break  # Exit the loop if the answer is 'no'
         elif will_add != "yes":
             print("Error! Please input <Yes, No>.")    
-# close file
