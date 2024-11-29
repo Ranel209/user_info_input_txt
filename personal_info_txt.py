3c74133a538362a17c9b209441b22de665a82271
Adrian Ranel <adrianranelm@gmail.com>
2024-11-30 01:32:36 +0800

    ask user to loop

--- personal_info_txt.py
+++ personal_info_txt.py
@@ -1,11 +1,24 @@
 # create a txt file to store data
-info_file = open ("./user_info_list.txt", "w")
-input_counter = 0
+with open ("./user_info_list.txt", "w") as info_file:
+
 # loop to ask user if they want to enter more information
-while True:
-# loop for 5 types of information
+
+    while True:
+
+# ask user for 5 types of information
+
+            
+
+                
+
+
 # insert user inputs into txt
-# close loop 1
-# close loop 2
+# close loop 
+        will_add = input("Would you like to continue<Yes/No> :")
+        will_add = will_add.lower()
+        if will_add == "no":
+            print("Goodbye!")
+            break  # Exit the loop if the answer is 'no'
+        elif will_add != "yes":
+            print("Error! Please input <Yes, No>.")    
 # close file
-    break
