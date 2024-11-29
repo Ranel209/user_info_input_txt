70078690d3c6ca01de7be179b8b7858244d36a26
Adrian Ranel <adrianranelm@gmail.com>
2024-11-30 02:09:27 +0800

    final?

--- personal_info_txt.py
+++ personal_info_txt.py
@@ -1,21 +1,40 @@
 # create a txt file to store data
 user_count = 0
-with open ("./user_info_list.txt", "w") as info_file:
-
-# loop to ask user if they want to enter more information
-
+with open("./user_info_list.txt", "w") as info_file:
     while True:
-#Count amount of users
+        # Count amount of users
         user_count += 1
         print(f"User {user_count}: ")
 
-        '''ask user for 5 types of information'''
+        '''Ask user for 5 types of information'''
 
-# close loop 
-        will_add = input("Would you like to continue<Yes/No> :")
+        # ask for user name
+        user_name = input("Input User Name: ")
+        info_file.write("Name: " + user_name + "\n")
+
+        # ask for user age
+        user_age = int(input("Input User Age: "))
+        info_file.write("Age: " + str(user_age) + "\n")
+
+        # ask user for favorite color
+        user_color = input("Input User Favorite Color: ")
+        info_file.write("Color: " + user_color + "\n")
+
+        # ask user for favorite programming language
+        user_language = input("Input User Favorite Programming Language: ")
+        info_file.write("Language: " + user_language + "\n")
+
+        # ask user for hobbies
+        user_hobby = input("Input User Favorite Hobby: ")
+        info_file.write("Hobby: " + user_hobby + "\n")
+
+        info_file.write("----------------------------------------------------------\n")
+
+        # Ask if they want to add another user
+        will_add = input("Would you like to continue <Yes/No> :")
         will_add = will_add.lower()
         if will_add == "no":
             print("Goodbye!")
             break  # Exit the loop if the answer is 'no'
         elif will_add != "yes":
-            print("Error! Please input <Yes, No>.")    
+            print("Error! Please input <Yes, No>.")
