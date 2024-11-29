143d2b3eb1b0e3844df2824b33c487d642e7b0fd
Adrian Ranel <adrianranelm@gmail.com>
2024-11-30 03:26:07 +0800

    opening file and starting loop

--- personal_info_finder.py
+++ personal_info_finder.py
@@ -1,6 +1,16 @@
 # opens file
+try:
+    with open("./user_info_list.txt", "r") as file:
+        entire_txt = file.readlines()
+        if_continue = "yes"
+        
+        while if_continue == "yes":
+            if_found = 0
+            find_name = input("Input The Name to get Information: ")
 #Start searching for name
 #loop to search for name
 #if name found print next few lines
 #move to next line if name not found
 #ask to continue the search
+except:
+      print("An error occurred")
